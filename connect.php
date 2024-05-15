299081f32fb32511922bc11922a0c77bab79f137
KenichiLei <154303556+KenichiLei@users.noreply.github.com>
2024-05-15 10:12:02 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,30 +7,39 @@
     $conn = "";
 
 
-    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-    
-    if (!$conn) {
-        die ("Luh, Anong ginawa mo???" . mysqli_connect_error());
-    } else {
-        echo "Connected Successfully!</br>";
+    try{
+
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+    }catch(mysqli_sql_exception){
+        echo "Error: Connection Lost!";
+
     }
 
-    $uname = $_POST['uname'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $pnumber = $_POST['pnumber'];
+    $uname = mysqli_real_escape_string($conn, $_POST['uname']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pnumber = mysqli_real_escape_string($conn, $_POST['pnumber']);
 
 
     $sql = "INSERT INTO customerform (Username, password, EmailAddress, PhoneNumber)
             VALUES ('$uname', '$email', '$pnumber', '$password')";
 
+    try {
+        if(mysqli_query($conn, $sql)) {
+            echo "New Record created successfully";
 
-    if(mysqli_query($conn, $sql)) {
-        echo "New Record created successfully";
+        } else {
+            echo "Error . " . $sql. "<br>" . mysqli_error($conn);
 
-    } else {
-        echo "Error . " . $sql. "<br>" . mysqli_error($conn);
+        }
+
+
+    }catch(mysqli_sql_exception $e) {
+        echo "ERROR: MALI ANG CODE";
 
     }
+
+
     mysqli_close($conn);
 ?>
