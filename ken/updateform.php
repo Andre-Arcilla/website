0c550575c949a6919bc0fe02e929d6aeecbfe7d2
KenichiLei <154303556+KenichiLei@users.noreply.github.com>
2024-05-16 19:00:55 +0800

    Add files via upload

--- ken/updateform.php
+++ ken/updateform.php
@@ -8,11 +8,11 @@
 <body>
     <h3>Update</h3>
     <hr><form action="update.php" method="POST">
-        <p><b>Enter Record Number to Update <br> </b> <input type="text" name="Anumber" size="3"></p>
-        <p><b>Enter new Username <br> </b> <input type="text" name="uname" size="40"></p>
-        <p><b>Enter new Password <br> </b> <input type="text" name="password" size="40"></p>
-        <p><b>Enter new Phone Number <br> </b> <input type="text" name="pnumber" size="40"></p>
-        <p><b>Enter new Email Address <br> </b> <input type="text" name="email" size="40"></p>
+        <p><b>Enter Record Number to Update <br> </b> <input type="text" name="AccountNumber" size="3"></p>
+        <p><b>Enter new Username <br> </b> <input type="text" name="username" size="40"></p>
+        <p><b>Enter new Password <br> </b> <input type="text" name="passWord" size="40"></p>
+        <p><b>Enter new Phone Number <br> </b> <input type="text" name="phoneNumber" size="40"></p>
+        <p><b>Enter new Email Address <br> </b> <input type="text" name="emailAddress" size="40"></p>
         <input type="submit" value="Submit" name="submit">
 
     </form>
