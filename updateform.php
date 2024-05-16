de621e5bde95d27eb4e0cbdd70a9c92c5b6a99df
KenichiLei <154303556+KenichiLei@users.noreply.github.com>
2024-05-16 17:52:30 +0800

    Update updateform.php

--- updateform.php
+++ updateform.php
@@ -9,9 +9,9 @@
     <h3>Update</h3>
     <hr><form action="update.php" method="POST">
         <p><b>Enter Record Number to Update <br> </b> <input type="text" name="Anumber" size="3"></p>
-        <p><b>Enter new Username <br> </b> <input type="text" name="lname" size="40"></p>
-        <p><b>Enter new Password <br> </b> <input type="text" name="lname" size="40"></p>
-        <p><b>Enter new Phone Number <br> </b> <input type="text" name="lname" size="40"></p>
+        <p><b>Enter new Username <br> </b> <input type="text" name="uname" size="40"></p>
+        <p><b>Enter new Password <br> </b> <input type="text" name="password" size="40"></p>
+        <p><b>Enter new Phone Number <br> </b> <input type="text" name="pnumber" size="40"></p>
         <p><b>Enter new Email Address <br> </b> <input type="text" name="email" size="40"></p>
         <input type="submit" value="Submit" name="submit">
 
