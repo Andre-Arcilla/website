01bbfc04ae823a0853234273f09e9ab1eb3d2ebb
KenichiLei <154303556+KenichiLei@users.noreply.github.com>
2024-05-19 01:32:45 +0800

    Add files via upload

---
+++ ken/tracking.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2024 at 07:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tracking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gps_track`
+--
+
+CREATE TABLE `gps_track` (
+  `IDRider` bigint(20) NOT NULL,
+  `trackTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `longitude` decimal(11,0) NOT NULL,
+  `latitude` decimal(11,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gps_track`
+--
+ALTER TABLE `gps_track`
+  ADD PRIMARY KEY (`IDRider`),
+  ADD KEY `trackTime` (`trackTime`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
